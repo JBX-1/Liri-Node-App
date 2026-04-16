b197606c9e5745a6acae6d729986e24b8ee60a49
Jonathan X <39885176+JBX-1@users.noreply.github.com>
2026-04-16 07:29:04 -0400

    Refactor liri.js for improved readability and structure

--- liri.js
+++ liri.js
@@ -1,111 +1,174 @@
-var request = require("request");
-var dotenv = require("dotenv").config();
-var keys = require("./keys.js");
-var fs = require("fs");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+require("dotenv").config();
 
+const fs = require("fs");
+const request = require("request");
+const Spotify = require("node-spotify-api");
+const keys = require("./keys.js");
 
+const spotify = new Spotify(keys.spotify);
 
-var argument = process.argv[2];
-var input = process.argv.slice(3).join("-");
+const command = process.argv[2];
+const userInput = process.argv.slice(3).join(" ").trim();
 
+function runLiri(action, input) {
+  switch (action) {
+    case "concert-this":
+      bandsInTown(input);
+      break;
 
-function liri() {
-if(argument === "concert-this") {
-    console.log(input);
-    bandsInTown(input);
+    case "spotify-this-song":
+      searchSpotify(input);
+      break;
 
-}else if(argument === "spotify-this-song") {
-    console.log("");
-    searchSpotify(input);
+    case "movie-this":
+      searchOmdb(input);
+      break;
 
-} else if (argument === "movie-this") {
-    console.log(input);
-    searchOmdb(input);
+    case "do-what-it-says":
+      whateverItSays();
+      break;
 
-} else if (argument === "do-what-it-says") {
-    console.log("");
-    whateverItSays(input);
-    
+    default:
+      console.log(`
+Please use one of these commands:
+- node liri.js concert-this "Artist Name"
+- node liri.js spotify-this-song "Song Name"
+- node liri.js movie-this "Movie Name"
+- node liri.js do-what-it-says
+      `);
+  }
 }
-};
-
-liri();
-
 
 function bandsInTown(artist) {
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
-        
-        // console.log(response)
-        
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-      
-          // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        //   console.log(JSON.parse(body));
-          console.log(JSON.parse(body)[0].venue.name)
-          console.log(JSON.parse(body)[0].venue.city)
-          console.log(JSON.parse(body)[0].datetime)
+  const artistName = artist || "Drake";
+  const queryUrl = `https://rest.bandsintown.com/artists/${encodeURIComponent(
+    artistName
+  )}/events?app_id=codingbootcamp`;
 
-          var parsedInfo = JSON.parse(body);
-
-        }
-      });
-    
-}
-// spotify search only got it give me the name of who wrote the song//
-function searchSpotify (song) {
-    spotify
-        .search({ type: "track", query: song, limit: 1 }, function (err, data) {
-            if (err) {
-                return console.log("Error occurred: " + err);
-            }
-            else {
-                console.log("artist: " + data.tracks.items[0].album.artists[0].name);
-			}
-        });
-        console.log(searchOmdb)
-}
-
-
-// i put in where it only gives me space jam//
-function searchOmdb () {
-
-    var movieName =  "http://www.omdbapi.com/?t= space jam  &y=&plot=short&apikey=trilogy";
-
-    if (!input) {
-        input = "terminator";
+  request(queryUrl, (error, response, body) => {
+    if (error) {
+      console.log("Error fetching concert data:", error);
+      return;
     }
 
-    request(movieName, function(error, response, body) {
+    if (response.statusCode !== 200) {
+      console.log("Unable to retrieve concert data.");
+      return;
+    }
 
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-      
-          // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        //   console.log("Release Year: " + JSON.parse(body).Year);
+    const data = JSON.parse(body);
 
-        //   var parsedInfo = JSON.parse(body);
-          
-          console.log("Title: " + JSON.parse(body).Released);
-          
-        
+    if (!Array.isArray(data) || data.length === 0) {
+      console.log("No upcoming concerts found.");
+      return;
+    }
 
-        }
-      });
+    console.log("\n--- Concert Information ---");
+    console.log("Venue:", data[0].venue.name);
+    console.log("Location:", `${data[0].venue.city}, ${data[0].venue.country}`);
+    console.log("Date:", data[0].datetime);
+  });
 }
 
-// doesnt take in the information i put in //
+function searchSpotify(song) {
+  const songName = song || "The Sign Ace of Base";
+
+  spotify.search({ type: "track", query: songName, limit: 1 }, (err, data) => {
+    if (err) {
+      console.log("Spotify error:", err);
+      return;
+    }
+
+    if (
+      !data ||
+      !data.tracks ||
+      !data.tracks.items ||
+      data.tracks.items.length === 0
+    ) {
+      console.log("No song found.");
+      return;
+    }
+
+    const track = data.tracks.items[0];
+
+    console.log("\n--- Song Information ---");
+    console.log(
+      "Artist(s):",
+      track.artists.map((artist) => artist.name).join(", ")
+    );
+    console.log("Song Name:", track.name);
+    console.log("Preview URL:", track.preview_url || "No preview available");
+    console.log("Album:", track.album.name);
+  });
+}
+
+function searchOmdb(movie) {
+  const movieName = movie || "Mr. Nobody";
+  const queryUrl = `http://www.omdbapi.com/?t=${encodeURIComponent(
+    movieName
+  )}&plot=short&apikey=trilogy`;
+
+  request(queryUrl, (error, response, body) => {
+    if (error) {
+      console.log("Error fetching movie data:", error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.log("Unable to retrieve movie data.");
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.Response === "False") {
+      console.log("Movie not found.");
+      return;
+    }
+
+    console.log("\n--- Movie Information ---");
+    console.log("Title:", data.Title);
+    console.log("Year:", data.Year);
+    console.log("IMDB Rating:", data.imdbRating);
+    console.log("Rotten Tomatoes Rating:", getRottenTomatoesRating(data.Ratings));
+    console.log("Country:", data.Country);
+    console.log("Language:", data.Language);
+    console.log("Plot:", data.Plot);
+    console.log("Actors:", data.Actors);
+  });
+}
+
+function getRottenTomatoesRating(ratings) {
+  if (!ratings || !Array.isArray(ratings)) {
+    return "N/A";
+  }
+
+  const rottenTomatoes = ratings.find(
+    (rating) => rating.Source === "Rotten Tomatoes"
+  );
+
+  return rottenTomatoes ? rottenTomatoes.Value : "N/A";
+}
+
 function whateverItSays() {
-    
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        // If the code experiences any errors it will log the error to the console.
-        if (error) {
-          return console.log(error);
-        }
- });
+  fs.readFile("random.txt", "utf8", (error, data) => {
+    if (error) {
+      console.log("Error reading random.txt:", error);
+      return;
+    }
+
+    const parts = data.split(",");
+
+    if (parts.length < 2) {
+      console.log("random.txt is not formatted correctly.");
+      return;
+    }
+
+    const fileCommand = parts[0].trim();
+    const fileInput = parts[1].replace(/"/g, "").trim();
+
+    runLiri(fileCommand, fileInput);
+  });
 }
 
+runLiri(command, userInput);
